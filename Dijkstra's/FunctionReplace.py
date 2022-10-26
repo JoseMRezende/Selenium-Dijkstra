8c8e01b9173a93d2db12d1a5714472780f871182
Jose Rezende <rezendinhobr18@gmail.com>
2022-10-26 20:01:23 -0300

    one

---
+++ Dijkstra's/FunctionReplace.py
@@ -0,0 +1,3 @@
+python = "Room $$Horizontal$$ Python"                        #example one
+FunctionReplace = python.replace("$$Horizontal$$","Update_Letter")       #answer example one
+print(FunctionReplace)
---
+++ Dijkstra's/Selenium.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import random
+
+nav = webdriver.Chrome(executable_path="C:\\Users\\Rezende\\Documents\\chromedriver.exe")
+nav.maximize_window()
+nav.get('https://ng-pathfinder.netlify.app')
+time.sleep(2)
+PrimaryBlock = '/html/body/app-root/app-board/div/div[$$Vertical$$]/div[$$Horizontal$$]/app-node/div'
+ButtonPlay = '/html/body/app-root/app-header/div/button'
+counter = 0
+stop = False
+while stop == False:
+    x = random.randrange(1,46)
+    y = random.randrange(1,21)
+    h = str(x)
+    v = str(y)
+    counter += 1
+    if counter == 450:
+        time.sleep(2)
+        nav.find_element(By.XPATH,ButtonPlay).click()
+        time.sleep(15)
+        nav.find_element(By.XPATH,ButtonPlay).click()
+        time.sleep(1)
+        break
+    RandomClickOne = PrimaryBlock.replace("$$Horizontal$$",h)
+    RandomClickTwo = RandomClickOne.replace("$$Vertical$$",v)
+    nav.find_element(By.XPATH,RandomClickTwo).click()
